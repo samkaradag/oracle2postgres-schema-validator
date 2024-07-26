cf5056ec33523ae8408545f8a9d154478cda749c
sametkaradag <sametkaradag@gmail.com>
2024-07-26 12:30:27 +0200

    changes

--- importer/importer.py
+++ importer/importer.py
@@ -18,6 +18,13 @@ import os
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 import zipfile
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+import pandas as pd
+
+# Base = declarative_base()
 
 
 def unzip_all_files(directory_path):
@@ -99,16 +106,46 @@ def load_csv_to_bigquery(project_id, dataset_id, csv_directory, location="US"):
     # Load CSV files
     load_csv_files(client, project_id, dataset_id, csv_directory)
 
+def load_csv_to_postgres(csv_directory, postgres_connection_string):
+    """Loads CSV files into the specified PostgreSQL database."""
+    # dbschema='schema-compare' # Searches left-to-right
+    # engine = create_engine(postgres_connection_string, connect_args={'options': '-csearch_path={}'.format(dbschema)})
+    engine = create_engine(postgres_connection_string)
+    # Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+
+    for filename in os.listdir(csv_directory):
+        if filename.endswith(".csv") and (not  ("defines" in filename or "eoj" in filename)):
+            file_path = os.path.join(csv_directory, filename)
+            table_name = filename.split("__")[1]
+            try:
+                # Attempt to read with comma delimiter first
+                df = pd.read_csv(file_path, sep='|')
+                # # Convert columns to lowercase
+                df.columns = df.columns.str.lower()
+
+                df.to_sql(table_name, engine, if_exists='append', index=False)
+                print(f"Loaded {filename} into PostgreSQL.")
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Load CSV files into BigQuery tables.")
-    parser.add_argument("--project_id", help="Your Google Cloud Project ID.")
+    parser = argparse.ArgumentParser(description="Load CSV files into BigQuery or PostgreSQL tables.")
+    parser.add_argument("--project_id", help="Your Google Cloud Project ID (for BigQuery).")
     parser.add_argument("--dataset_id", help="The BigQuery dataset name.")
     parser.add_argument("--csv_directory", default="./extracts", help="Directory containing CSV files.")
-    parser.add_argument("--zip_directory", default="./extracts", help="Directory containing CSV files.")
+    parser.add_argument("--zip_directory", default="./extracts", help="Directory containing ZIP files.")
     parser.add_argument("--location", default="US", help="Geographic location for the dataset (default: US)")
+    parser.add_argument("--postgres_connection_string", help="Connection string for your PostgreSQL database (optional).")
     args = parser.parse_args()
 
     unzip_all_files(args.zip_directory)
 
-    load_csv_to_bigquery(args.project_id, args.dataset_id, args.csv_directory, args.location)
+    if args.project_id and args.dataset_id:
+        load_csv_to_bigquery(args.project_id, args.dataset_id, args.csv_directory, args.location)
+
+    if args.postgres_connection_string:
+        load_csv_to_postgres(args.csv_directory, args.postgres_connection_string)
