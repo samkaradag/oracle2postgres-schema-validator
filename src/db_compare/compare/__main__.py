9506c3fd93e4d9909dcf0613000f83bfaef700ec
sametkaradag <sametkaradag@gmail.com>
2024-10-18 10:55:36 +0200

    changes

--- src/db_compare/compare/__main__.py
+++ src/db_compare/compare/__main__.py
@@ -143,7 +143,7 @@ def main():
                         "--view_type", args.oracle_view_type]
                 command.extend(arguments)
 
-                result = subprocess.run(command, check=True, capture_output=True, text=True)
+                result = subprocess.run(command, check=True)
                 # Print the captured output
                 print(result.stdout)
                 # print(f"Oracle metadata extraction successful for connection {i}.")
